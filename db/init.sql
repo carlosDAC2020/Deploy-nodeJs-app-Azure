d218f4b9773a27474f616bbb3e53388af8683f41
Carlos Agamez <carlos.agamez@sigma3ds.com>
2024-04-17 17:15:20 -0500

    buld image of database

---
+++ db/init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE testdb;
+
+USE testdb;
+
+CREATE TABLE IF NOT EXISTS `tutorials` (
+  id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  title varchar(255) NOT NULL,
+  description varchar(255),
+  published BOOLEAN DEFAULT false
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
